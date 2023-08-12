aec43d90a867f1783108e4b8f36072a2a484c2ad
HaruoSydney <qingpeng256@gmail.com>
2023-08-12 17:40:29 +0900

    1. Fix the java-1.5 issue 2. Add service for product

--- mall-product/src/main/java/com/pq/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/pq/mall/product/MallProductApplication.java
@@ -1,9 +1,12 @@
 package com.pq.mall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 指定Mapper接口对应的路径
+@MapperScan("com.pq.mall.product.dao")
 public class MallProductApplication {
 
     public static void main(String[] args) {
--- mall-product/src/test/java/com/pq/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/pq/mall/product/MallProductApplicationTests.java
@@ -1,13 +1,32 @@
 package com.pq.mall.product;
 
+import com.pq.mall.product.entity.BrandEntity;
+import com.pq.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MallProductApplicationTests {
+    @Autowired
+    BrandService brandService;
 
     @Test
     void contextLoads() {
+        BrandEntity entity = new BrandEntity();
+        entity.setName("XiaoMi");
+        brandService.save(entity);
+    }
+
+    @Test
+    void selectAll(){
+        List<BrandEntity> list = brandService.list();
+        for (BrandEntity brandEntity : list) {
+            System.out.println(brandEntity);
+        }
+
     }
 
 }
